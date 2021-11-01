1944480e884e46e4b0794b036c53b5d6f363d998
Tushar <tm2582001@gmail.com>
2021-11-01 16:03:52 +0530

    fixed a bug caused due to large json data

--- server-side/routes/locationData.js
+++ server-side/routes/locationData.js
@@ -2,26 +2,34 @@ const express = require("express");
 const router = express.Router();
 const https = require('https');
 
-router.get("/",(req,res)=>{
+router.get("/", (req, res) => {
     let coords = req.query.coords;
     let key = process.env.LOCATIONAPI;
-    let url = "https://api.opencagedata.com/geocode/v1/json?q="+coords+"&key="+key;
-    
-    https.get(url,(response)=>{
-        response.on('data', (d)=>{
-            let data = JSON.parse(d);
-            
-            let {city,state_district} = data.results[0].components;
-            
+    let url = "https://api.opencagedata.com/geocode/v1/json?q=" + coords + "&key=" + key;
+
+
+    https.get(url, (response) => {
+        let result = '';
+        response.on("data", (data) => {
+            result += data;
+        });
+
+        // we need end because it is a large json data it requires time
+        
+        response.on('end', () => {
+            let locationData = JSON.parse(result);
+            let { city, state_district } = locationData.results[0].components;
+
             state_district = state_district.replace(" District", '');
-            
-            if(city){
-                res.send(city)
-            }else if(state_district){
+
+            if (city) {
+                res.send(city);
+            } else if (state_district) {
                 res.send(state_district);
             }
-
         });
+
+
     });
 
 });
