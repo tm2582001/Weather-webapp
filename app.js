ac2e41a08d4d748aee2ffe740a8ff6a639490ed4
Tushar <tm2582001@gmail.com>
2021-11-01 13:07:26 +0530

    implemented geolocation api to detect co-ordinates
    implement OpenCage api to get location

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+require('dotenv').config()
 const express = require("express");
 const ejs = require("ejs");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
+
 app.set('view engine','ejs');
 
-
 const dataRoute = require("./routes/weatherData");
-
+const locationDataroute = require("./routes/locationData");
 
 app.use('/',dataRoute);
+app.use('/location',locationDataroute);
+
 
 app.listen(8080,()=>{
     console.log("server started at 8080");
---
+++ routes/locationData.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const https = require('https');
+
+router.get("/",(req,res)=>{
+    let coords = req.query.coords;
+    let key = process.env.LOCATIONAPI;
+    let url = "https://api.opencagedata.com/geocode/v1/json?q="+coords+"&key="+key;
+    
+    https.get(url,(response)=>{
+        response.on('data', (d)=>{
+            let data = JSON.parse(d);
+            console.log(data.results[0]);
+            let {city,state_district} = data.results[0].components;
+            
+            state_district = state_district.replace(" District", '');
+            
+            if(city){
+                res.send(city)
+            }else if(state_district){
+                res.send(state_district);
+            }
+
+        });
+    });
+
+});
+
+module.exports = router;
